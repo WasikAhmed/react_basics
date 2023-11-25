1985ae46ec7de0c8739b00ed67d70101eef5e1ab
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-11-25 07:25:41 +0600

    Add CORS configuration for front-end access (#5)

--- student_crud/student_crud/settings.py
+++ student_crud/student_crud/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "backend",
     "rest_framework",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  
 ]
 
 ROOT_URLCONF = "student_crud.urls"
