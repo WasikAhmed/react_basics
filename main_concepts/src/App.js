ffb7acd0239e1edbd259cf0f962ba15970bce7aa
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-12-21 12:24:45 +0600

    Update 'main_concepts' Add: Fetch student details from api using useEffect Hook

--- main_concepts/src/App.js
+++ main_concepts/src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UseStateHook from "./components/hooks/useStateHook";
 import UseEffectHook from './components/hooks/useEffectHook';
+import StudentList from './components/StudentList';
 
 const App = () => {
   return (
@@ -8,6 +9,8 @@ const App = () => {
       <UseStateHook />
       <hr></hr>
       <UseEffectHook />
+      <hr></hr>
+      <StudentList />
     </>
   );
 }
